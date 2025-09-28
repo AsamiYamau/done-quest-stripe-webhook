bf903c4f1908b6f3bb936224e5dbaaeff579ce81
YamauchiAsami <>
2025-09-28 13:52:58 +0900

    デバッグ

--- app/api/webhook-stripe/route.ts
+++ app/api/webhook-stripe/route.ts
@@ -13,7 +13,7 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2025-08-27.basil",
 });
-
+console.log(process.env.STRIPE_WEBHOOK_SECRET);
 export async function POST(req: NextRequest) {
   const buf = Buffer.from(await req.arrayBuffer()); // ← 型エラー解消
   const sig = req.headers.get("stripe-signature")!;
